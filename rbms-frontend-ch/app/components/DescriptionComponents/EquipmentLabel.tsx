c2a7a216ec765aac65dc90db55937ffb66a5fdf4
cheehongtee <162426959+cheehongtee@users.noreply.github.com>
2024-03-07 23:07:48 +0800

    Adding frontend files

---
+++ rbms-frontend-ch/app/components/DescriptionComponents/EquipmentLabel.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+interface EquipmentTuple {
+  name: string;
+  amount: number;
+}
+
+interface EquipmentProps {
+  equipment: EquipmentTuple[];
+}
+
+const EquipmentLabel: React.FC<EquipmentProps> = ({ equipment }) => {
+  return (
+    <div className="flex flex-row">
+      <div>
+        <label className="text-sm pr-4">Equipment</label>
+      </div>
+
+      <div className="flex flex-grow flex-col">
+        {equipment.map((eqpt, index) => (
+          <div key={index} className="flex flex-row">
+            <div className="w-1/2">
+              <label className="text-sm font-semibold">{eqpt.name}</label>
+            </div>
+
+            <div className="w-1/2">
+              <label className="text-sm text-gray-400">x </label>
+              <label className="text-sm text-gray-400">{eqpt.amount}</label>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default EquipmentLabel;
