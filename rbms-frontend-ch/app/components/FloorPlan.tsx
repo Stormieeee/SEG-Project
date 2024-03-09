0c1748455c560b945e2726e741318bd6a73672d3
cheehongtee <162426959+cheehongtee@users.noreply.github.com>
2024-03-09 15:47:50 +0800

    Reuploading folder

---
+++ rbms-frontend-ch/app/components/FloorPlan.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FloorPlan = () => {
+  return (
+    <div className="bg-white border border-gray-300 rounded-2xl h-full p-5">
+      <div>Test</div>
+    </div>
+  );
+};
+
+export default FloorPlan;
