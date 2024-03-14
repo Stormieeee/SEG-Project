5eacc4e2eba509cf036eb1391d10206bcd1b88ef
Jieyang02 <injieyang@gmail.com>
2024-03-14 14:34:18 +0800

    Add confirm button

--- frontend/src/app/(main)/Topbar.tsx
+++ frontend/src/app/(main)/Topbar.tsx
@@ -1,7 +1,29 @@
+"use client";
+import ConfirmButton from "./roombooking/ConfirmButton";
+import { usePathname } from "next/navigation";
+
+const getPageTitle = (path: string): string => {
+  switch (path) {
+    case '/roombooking':
+      return 'Room Booking';
+    case '/mybooking':
+      return 'My Booking';
+    case '/profile':
+      return 'Profile';
+    default:
+      return 'Default Title';
+  }
+};
+
 const Topbar = () => {
+  const pathname = usePathname();
+  const pageTitle = getPageTitle(pathname);
   return (
-    <div className="flex w-full h-30 bg-white shadow-sm items-center p-5">
-      <h1 className="font-bold text-2xl">Room Booking</h1>
+    <div className="flex w-full h-20 bg-white border-b border-gray-300 items-center p-5 shadow-md">
+      <div className="flex w-full justify-between">
+        <h1 className="font-bold text-2xl">{pageTitle}</h1>
+        {pathname === "/roombooking" && <ConfirmButton />}
+      </div>
     </div>
   );
 };
---
+++ frontend/src/app/(main)/roombooking/ConfirmButton.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+
+const ConfirmButton = () => {
+    
+    return (
+        <button className="w-40 h-10 bg-cyan-500 rounded-[20px] text-white-50">
+            Confirm
+        </button>
+    );
+}
+export default ConfirmButton;
