5750b48d49157cb48d99e0e331abbc1d9e60dea1
Louise-Yee <yeezongze@gmail.com>
2024-03-28 21:44:07 +0800

    Update page.tsx in root folder(enable logout)

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,39 @@
-import LoginForm from "./LoginForm/page";
+"use client";
+import LoginForm from "./Components/LoginForm";
+import Authentication from "./Components/Authentication";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Page() {
+  const router = useRouter();
+  const [showLogin, setShowLogin] = useState<boolean>(true);
+  const [showAuth, setShowAuth] = useState<boolean>(false);
+  const handleSuccessAuth = () => {
+    setShowAuth(false);
+    router.push("/roombooking");
+  };
+  const handleFailedAuth = () => {
+    setShowAuth(false);
+    setShowLogin(true);
+  };
+  const handleSuccessLogin = () => {
+    setShowLogin(false);
+    setShowAuth(true);
+  };
+  const handleCancelAuth = () => {
+    setShowAuth(false);
+    setShowLogin(true);
+  };
   return (
     <div className="flex flex-1">
-      <LoginForm />
+      {showAuth && (
+        <Authentication
+          handleSuccessAuth={handleSuccessAuth}
+          handleFailedAuth={handleFailedAuth}
+          handleCancelAuth={handleCancelAuth}
+        />
+      )}
+      {showLogin && <LoginForm handleSuccessLogin={handleSuccessLogin} />}
     </div>
   );
 }
